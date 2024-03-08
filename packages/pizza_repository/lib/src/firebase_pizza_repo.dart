1bb0b9b53b45e9c89218d94c9159f83f0933480f
Prateek Thakur <prateekthakur272@gmail.com>
2024-03-09 00:48:57 +0530

    adds firebase repo

---
+++ packages/pizza_repository/lib/src/firebase_pizza_repo.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:pizza_repository/src/pizza_repo.dart';
+import 'models/models.dart';
+
+class FirebasePizzaRepo implements PizzaRepo {
+  final pizzaCollection = FirebaseFirestore.instance.collection('pizzas');
+
+  @override
+  Future<List<Pizza>> getPizzas() async {
+    try {
+      final docs = (await pizzaCollection.get()).docs;
+      return docs.map((doc) => Pizza.fromMap(doc.data())).toList();
+    } catch (e) {
+      log(e.toString());
+      rethrow;
+    }
+  }
+}
